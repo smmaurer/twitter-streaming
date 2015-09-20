7b3686d8188ae39a01287c1b28096795f8426a6a
Sam Maurer <smmaurer@gmail.com>
2015-09-20 15:11:36 -0700

    Adds automatic reconnection

--- westcoast_stream.py
+++ westcoast_stream.py
@@ -8,57 +8,80 @@ from TwitterAPI import TwitterAPI
 from datetime import datetime as dt
 import json
 import time
+
 from keys import *   # keys.py in same directory
 
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 OUTPUT_PATH = 'data/'
 FNAME_BASE = 'westcoast-'
-TIME_LIMIT = 2  # in seconds, 0 for none
+TIME_LIMIT = 00  # in seconds, 0 for none
 ROWS_PER_FILE = 500000  # 500k tweets is about 1.6 GB uncompressed
 
 BBOX = '-126,29,-113,51'  # West Coast from Tijuana to Vancouver and east to edge of CA
 
-def stream():
-	r = api.request('statuses/filter', {'locations': BBOX})
 
-	tcount = 0 # tweets in file
-	t0 = time.time()
+def stream():
+	delay = 0.5  # initial reconnection delay in seconds
 
 	while True:
 		try:
-			for tweet in r.get_iterator():
+			r = api.request('statuses/filter', {'locations': BBOX, 'stall_warnings': 'true'})
+			_test = r.get_iterator()
+			print "Connected to streaming endpoint"
+			delay = 0.5  # reset the delay after a successful connection
+			save_tweets(r)
+			r.close()
+			return
 		
-				if (tcount == 0):
-					ts = dt.now().strftime('%Y%m%d-%H%M%S')
-					fname = OUTPUT_PATH + FNAME_BASE + ts + '.json'
-					f = open(fname, 'w')
-				
-				try:
-					f.write(json.dumps(tweet) + '\n')
-					tcount += 1
-				except:
-					pass
-				
-				if (tcount >= ROWS_PER_FILE):
-					f.close()
-					tcount = 0
-
-				if (TIME_LIMIT > 0) & ((time.time() - t0) >= TIME_LIMIT):
-					f.close()
-					break
-
-		except KeyboardInterrupt:
-			f.close()
-			break
-	
-		# Catch exceptions occasionally thrown by r.get_iterator() - eg ChunkedEncodingError
 		except Exception, e:
-			print e
+			# common: TwitterRequestError, TwitterConnectionError, httplib.IncompleteRead
+			r.close()  # try to close old stream
+			delay = delay * 2
+			print "%s: %s" % (type(e).__name__, e)
+			print "Attempting to reconnect after %d sec. delay" % (delay,)
+			time.sleep(delay)
 			continue
+
+
+def save_tweets(r):
+	tcount = 0 
+	t0 = time.time()
+
+	try:
+		for tweet in r.get_iterator():
+	
+			# log stall warnings
+			if 'warning' in tweet:
+				print tweet['warning']
+				continue
+
+			# initialize new output file if tweet count is 0
+			if (tcount == 0):
+				ts = dt.now().strftime('%Y%m%d-%H%M%S')
+				fname = OUTPUT_PATH + FNAME_BASE + ts + '.json'
+				f = open(fname, 'w')
 		
-		break
+			# save to file, or skip if the data is incomplete or has encoding errors
+			try:
+				f.write(json.dumps(tweet) + '\n')
+				tcount += 1
+			except:
+				continue
+		
+			# close the output file when it fills up
+			if (tcount >= ROWS_PER_FILE):
+				f.close()
+				tcount = 0
+
+			# if we reach a time limit, end the script
+			if (TIME_LIMIT > 0) & ((time.time() - t0) >= TIME_LIMIT):
+				f.close()
+				return
+
+	except KeyboardInterrupt:
+		f.close()
+		return
+
 
 stream()
-
-# to do - how to capture diagnostic info about stream quality?
